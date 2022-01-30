735bf9e9252271a40ae9650d815f15b80937b8bc
Natthawit Janjaroen <natthawit.jan@gmail.com>
2022-01-30 11:39:58 +0700

    Make another version of kthDigit whose function doesn't use any kind of loops

--- a3/digit.py
+++ a3/digit.py
@@ -8,46 +8,61 @@ def kthDigit(x: int, b: int, k: int):
 
 
 # make a version without using a loop
-
-def pp(ch: str):
-    print(ch, end='')
-
-
-def a():
-    pp('a')
+def kthDigitVersion2(x: int, b: int, k: int):
+    return x // (b ** k) % b if k >= 0 else 0
 
 
-def b():
-    pp('b')
+assert kthDigitVersion2(789, 10, 1) == 8
+assert kthDigitVersion2(789, 10, 2) == 7
+assert kthDigitVersion2(789, 10, 3) == 0
+assert kthDigitVersion2(789, 10, 0) == 9
+assert kthDigitVersion2(789, 10, -3) == 0
 
-
-def c():
-    a()
-    pp('c')
-
-
-def d():
-    pp('d')
-    b()
-
-
-def e():
-    """
-    d
-    """
-    c()
-    pp('e')
-    d()
-
-
-import itertools
-
-list_of_func = [a,b,c,d,e]
-
-all_possible_outcome = list(itertools.permutations(list_of_func, 5))
-
-for i in all_possible_outcome:
-    print(i)
-    for j in i:
-        j()
-    print()
+assert kthDigitVersion2(987, 16, 0) == 11
+assert kthDigitVersion2(987, 16, 1) == 13
+assert kthDigitVersion2(987, 16, 2) == 3
+assert kthDigitVersion2(987, 16, 3) == 0
+assert kthDigitVersion2(987, 16, 4) == 0
+assert kthDigitVersion2(987, 16, -323) == 0
+# def pp(ch: str):
+#     print(ch, end='')
+#
+#
+# def a():
+#     pp('a')
+#
+#
+# def b():
+#     pp('b')
+#
+#
+# def c():
+#     a()
+#     pp('c')
+#
+#
+# def d():
+#     pp('d')
+#     b()
+#
+#
+# def e():
+#     """
+#     d
+#     """
+#     c()
+#     pp('e')
+#     d()
+#
+#
+# import itertools
+#
+# list_of_func = [a,b,c,d,e]
+#
+# all_possible_outcome = list(itertools.permutations(list_of_func, 5))
+#
+# for i in all_possible_outcome:
+#     print(i)
+#     for j in i:
+#         j()
+#     print()
